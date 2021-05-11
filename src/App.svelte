7e657a6377b664c938c36786a2d9d64eda333a1d
Oliver Cederborg <3323050@rts-365.dk>
2021-05-11 15:13:06 +0200

    init

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+	import logo from './assets/svelte.png'
+	import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+	<h1 class="text-black">Hello world!</h1>
+
+	<Counter />
+</main>
