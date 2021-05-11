b96dbae4a1a1c8f959d7a213e5197045505e817e
Oliver Cederborg <3323050@rts-365.dk>
2021-05-12 00:31:04 +0200

    feat(search bar): create array

---
+++ src/items.js
@@ -0,0 +1,32 @@
+export const items = [
+	{
+		name: 'Coca Cola',
+		price: '10kr',
+		img: 'https://images.unsplash.com/photo-1554866585-cd94860890b7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1001&q=80'
+	},
+	{
+		name: 'Fanta',
+		price: '10kr',
+		img: 'https://images.unsplash.com/photo-1554866585-cd94860890b7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1001&q=80'
+	},
+	{
+		name: 'Sprite',
+		price: '10kr',
+		img: 'https://images.unsplash.com/photo-1554866585-cd94860890b7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1001&q=80'
+	},
+	{
+		name: 'Schweppes',
+		price: '10kr',
+		img: 'https://images.unsplash.com/photo-1554866585-cd94860890b7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1001&q=80'
+	},
+	{
+		name: 'Faxe Kondi',
+		price: '10kr',
+		img: 'https://images.unsplash.com/photo-1554866585-cd94860890b7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1001&q=80'
+	},
+	{
+		name: 'Pepsi',
+		price: '10kr',
+		img: 'https://images.unsplash.com/photo-1554866585-cd94860890b7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1001&q=80'
+	}
+]
