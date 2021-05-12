471484b0dcc23993d0b2a5564d50cef70677da3c
Oliver Cederborg <hi@olivercederborg.com>
2021-05-12 11:12:36 +0200

    feat(search bar): add component

--- src/App.svelte
+++ src/App.svelte
@@ -1,67 +1,26 @@
 <script>
-	import { items } from './items'
-	let searchValue
-	let searchResults = []
-
-	const checkName = (name, str) => {
-		var pattern = str
-			.split('')
-			.map((x) => {
-				return `(?=.*${x})`
-			})
-			.join('')
-		var regex = new RegExp(`${pattern}`, 'g')
-		return name.match(regex)
-	}
-
-	$: {
-		if (searchValue) {
-			let searchString = searchValue.toLowerCase().substring(0, 4)
-
-			searchResults = items.filter((item) => {
-				let substring = item.name.substring(0, 4).toLowerCase()
-				return (
-					item.name.toLowerCase().includes(searchString) ||
-					checkName(substring, searchString)
-				)
-			})
-		} else {
-			searchResults = []
-		}
-		console.log(searchResults)
-	}
+	import SearchBar from './lib/SearchBar.svelte'
 </script>
 
 <main>
-	<section class="container min-h-screen grid place-items-center">
-		<div class="relative">
-			<form>
-				<input
-					type="text"
-					bind:value={searchValue}
-					class="border py-3 px-4 rounded-lg"
-				/>
-				<button class="py-3 px-5 bg-black text-white rounded-lg"
-					>Search</button
-				>
-			</form>
-
-			{#if searchResults.length}
-				<ul
-					class="space-y-4 absolute top-20 bg-white shadow-lg w-full p-4 rounded-lg"
-				>
-					{#each searchResults as item}
-						<li class="flex items-center">
-							<img
-								src={item.img}
-								alt={item.name}
-								class="h-[50px] w-[50px] object-cover mr-4 rounded-lg"
-							/>
-							{item.name}: {item.price}
-						</li>
-					{/each}
-				</ul>
-			{/if}
-		</div>
+	<section
+		class="Search-section relative min-h-screen flex flex-col justify-center items-center bg-white"
+	>
+		<SearchBar />
 	</section>
 </main>
+
+<style>
+	.Search-section::after {
+		content: '';
+		position: absolute;
+		top: 0;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		opacity: 0.3;
+		background-image: url('https://images.unsplash.com/photo-1559056419-266cbf698202?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80');
+		background-size: cover;
+		filter: saturate(0);
+	}
+</style>
