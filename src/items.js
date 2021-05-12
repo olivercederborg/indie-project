5c01be6acc68b9a8a2d99e96c112c071cf21e807
Oliver Cederborg <hi@olivercederborg.com>
2021-05-12 11:15:10 +0200

    feat(search bar): update array items

--- src/items.js
+++ src/items.js
@@ -1,32 +1,32 @@
 export const items = [
 	{
 		name: 'Coca Cola',
-		price: '10kr',
+		price: '$1',
 		img: 'https://images.unsplash.com/photo-1554866585-cd94860890b7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1001&q=80'
 	},
 	{
 		name: 'Fanta',
-		price: '10kr',
-		img: 'https://images.unsplash.com/photo-1554866585-cd94860890b7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1001&q=80'
+		price: '$1',
+		img: 'https://assets.partyking.org/img/products/1300/fanta-orange-3.jpg'
 	},
 	{
 		name: 'Sprite',
-		price: '10kr',
-		img: 'https://images.unsplash.com/photo-1554866585-cd94860890b7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1001&q=80'
+		price: '$1',
+		img: 'https://images.heb.com/is/image/HEBGrocery/001397083'
 	},
 	{
 		name: 'Schweppes',
-		price: '10kr',
-		img: 'https://images.unsplash.com/photo-1554866585-cd94860890b7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1001&q=80'
+		price: '$1',
+		img: 'https://billigfadoel.dk/wp-content/uploads/2018/11/Schweppes-Lemon-25-cl-glasflaske-30-stk-fadoelsudlejning.png'
 	},
 	{
 		name: 'Faxe Kondi',
-		price: '10kr',
-		img: 'https://images.unsplash.com/photo-1554866585-cd94860890b7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1001&q=80'
+		price: '$1',
+		img: 'https://gutbier.dk/wp-content/uploads/2020/09/faxek500.jpg'
 	},
 	{
 		name: 'Pepsi',
-		price: '10kr',
-		img: 'https://images.unsplash.com/photo-1554866585-cd94860890b7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1001&q=80'
+		price: '$1',
+		img: 'https://sc04.alicdn.com/kf/U5223dc4eb60b49ea92e10cc40d37871di.jpg'
 	}
 ]
